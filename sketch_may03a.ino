267892d90acaf24e546482da40253168e690ab25
sparretti <sparretti@bancroftschool.org>
2018-05-03 10:43:32 -0400

    I did Push Buttons

---
+++ sketch_may03a.ino
@@ -0,0 +1,22 @@
+int button;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  button = digitalRead(8);
+  Serial.println(button);
+  if (button == 1) {
+     digitalWrite(7, HIGH);
+     delay(3000);
+     digitalWrite(7, LOW);
+  }
+
+}
