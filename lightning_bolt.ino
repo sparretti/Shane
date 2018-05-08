063169d6b820b2c09f8f58cfa081e1b23abc2037
sparretti <sparretti@bancroftschool.org>
2018-05-08 14:16:24 -0400

    Add files via upload
    completely finished coding

--- lightning_bolt.ino
+++ lightning_bolt.ino
@@ -19,21 +19,21 @@ void loop()
   Serial.println(button);
   if (button == 1) {
      digitalWrite(7, HIGH);
-     delay(3000);
-
+     delay(100);
      digitalWrite(6, HIGH);
-     delay(3000);
-     digitalWrite(6, LOW);
+     delay(100);
      digitalWrite(5, HIGH);
-     delay(3000);
-     digitalWrite(5, LOW);
+     delay(100);
      digitalWrite(4, HIGH);
-     delay(3000);
-     digitalWrite(4, LOW);
+     delay(100);
      digitalWrite(3, HIGH);
-     delay (3000);
+     delay(100);
      digitalWrite(3, LOW);
+     digitalWrite(4, LOW);
+     digitalWrite(5, LOW);
+     digitalWrite(6, LOW);
      digitalWrite(7, LOW);
+  
   }
 
 }
